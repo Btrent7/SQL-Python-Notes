e0ce4c96007caa1da0f19e18dafd9a08b4f3f0f4
Brandon Trent <185147333+Btrent7@users.noreply.github.com>
2025-09-24 15:29:08 -0500

    runningTotal_sales.sql

--- runningTotal_sales.sql
+++ runningTotal_sales.sql
@@ -1,6 +1,26 @@
+--RUNNING TOTAL SINGLE ITEM SALES
 SELECT invoiceDate, qtyShip, 
 		sum(qtyShip * unitPrice) OVER(ORDER BY invoiceDate ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as RT_Sales
 
-FROM DBO.InvoiceDetailHistory
+FROM dbo.InvSales INV
 
-WHERE item = '123456789'
+WHERE item = '123456789';
+
+--RUNNING TOTAL ALL SALES
+WITH SALES AS(
+SELECT INV.orderNum, INV.invoiceDate, INV.Item, INV.fiscalYear, INV.customerNumber,
+    SUM(INV.qtyShip * INV.unitPrice) as Revenue,
+	SUM(INV.costperiodEndReturn * qtyShip) as COGS
+FROM dbo.InvSales INV
+
+GROUP BY INV.orderNum, INV.invoiceDate, INV.Item, INV.fiscalYear, INV.customerNumber
+)
+
+SELECT *, 
+	SUM(Revenue) OVER (
+        PARTITION BY customerNumber
+        ORDER BY invoiceDate
+        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+    ) AS RunningTotal_SALES
+FROM SALES
+WHERE fiscalYear = '2025'
